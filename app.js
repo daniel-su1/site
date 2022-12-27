af241a4178a018a94dd84824ec4708409b7b1465
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2022-12-26 23:47:47 -0500

    Add footer

--- app.js
+++ app.js
@@ -2,42 +2,40 @@
 const elements = document.querySelectorAll('.fade-in');
 
 // get the window's current scroll position
-const scrollTop = window.pageYOffset;
+const scrollTop = window.scrollY;
 
 // get the window's height
 const windowHeight = window.innerHeight;
 
-// loop through each element with the "fade-in" class
-elements.forEach((element, index) => {
-  // set the element's initial opacity to 0
+async function changeClass(){
+  elements.forEach(async (element, index) => {
+    // set the element's initial opacity to 0
 
-  // get the element's distance from the top of the page
-  const elementTop = element.getBoundingClientRect().top;
-
-  // if the element is within the window's viewable area, add the "visible" class to the element
-  if (elementTop <= windowHeight) {
-    element.classList.add('visible');
-  }
-});
-
-
-
-// listen for the scroll event and run the above script whenever the user scrolls
-window.addEventListener('scroll', () => {
-  // get the window's current scroll position
-  const scrollTop = window.pageYOffset;
-
-  // get the window's height
-  const windowHeight = window.innerHeight;
-
-  // loop through each element with the "fade-in" class
-  elements.forEach(element => {
     // get the element's distance from the top of the page
     const elementTop = element.getBoundingClientRect().top;
-
+  
     // if the element is within the window's viewable area, add the "visible" class to the element
     if (elementTop <= windowHeight) {
       element.classList.add('visible');
+      document.addEventListener('animationend', () => {
+        element.classList.remove('hidden');
+      })
     }
+    
+
+
+
   });
-});
+  // loop through each element with the "fade-in" class
+  
+}
+
+// listen for the scroll event and run the above script whenever the user scrolls
+
+
+
+
+
+document.addEventListener('scroll', changeClass);
+changeClass();
+
