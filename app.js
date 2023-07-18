73d1332d9fe1172a1ba4ea1a6780856e10184a56
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-07-18 04:08:33 -0400

    updated animation using interaction observer api

--- app.js
+++ app.js
@@ -1,64 +1,22 @@
-// select all elements that have the "fade-in" class
-const elements = document.querySelectorAll('.fade-in');
-
-
-// get the window's current scroll position
-const scrollTop = window.scrollY;
-
-// get the window's height
-const windowHeight = window.innerHeight;
-
-function changeElement(element, delay) {
-  const elementTop = element.getBoundingClientRect().top;
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (elementTop <= windowHeight) {
-        element.classList.add('visible');
-        element.onanimationend = () => { element.classList.remove('hidden'); }
-      }
-      resolve();
-    }, delay)
-
-  })
-}
-
-async function changeClass() {
-  console.log("scroll " + window.scrollY);
-  console.log("height "+ window.innerHeight);
-  for (let i = 0; i != elements.length; i++) {
-
-    if(i>=4 && i<=17){
-      await changeElement(elements[i], 50);
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    // If the element is in viewport
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = "1";
+      // Once the element is in view, there's no need to keep observing it
+      observer.unobserve(entry.target);
     }
-    else{
-      await changeElement(elements[i], 150);
-    }
-  }
-}
+  });
+}, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5
+});
 
-function getTopVisibleElement(){
-  for (let i =0; i!= elements.length; i++){
-    if(elements[i].getBoundingClientRect().top <= windowHeight && elements[i].getBoundingClientRect().top > 0){
-      break;
-    }
-    return i;
-  }
-}
+// target all elements to observe
+let elements = document.querySelectorAll('.fade-in-element');
 
-function changeParticlesOpacity(){
-  let opacityLayer = document.querySelector('#opacity-layer')
-  if(window.scrollY>450){
-    opacityLayer.style.opacity = (window.scrollY-450)/((window.innerHeight*2.5)/10)
-  }
-  else{
-    opacityLayer.style.opacity = 0;
-
-  }
-
-}
-
-
-document.addEventListener('scroll', changeClass);
-document.addEventListener('scroll', changeParticlesOpacity)
-changeClass();
-
+// Observe each element
+elements.forEach(element => {
+  observer.observe(element);
+});
